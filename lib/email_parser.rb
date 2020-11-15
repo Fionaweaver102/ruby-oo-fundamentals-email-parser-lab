ffd9601927ce9cd8b84a36a3eea23340e8f63c23
Fiona Weaver <fiona.l.weaver@gmail.com>
2020-11-15 12:06:34 -0700

    commit tree c9f69cb8f15481fa6af03c0a80b5cc2611234fec

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,3 +1,17 @@
+class EmailAddressParser
+    attr_accessor :cvs_emails
+
+    def initialize(cvs_emails)
+        @cvs_emails = cvs_emails
+    end 
+
+    def parse 
+        cvs_emails.split.map do |addresses|
+            addresses.split(",")
+        end 
+        .flatten.uniq 
+    end
+end
 # Build a class EmailAddressParser that accepts a string of unformatted 
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
